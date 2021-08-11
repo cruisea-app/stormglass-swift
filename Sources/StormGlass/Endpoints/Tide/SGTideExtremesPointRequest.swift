b520b81648b0b1e780f5c5e3c84f5f9ffab1d8ac
James Sherlock <15193942+Sherlouk@users.noreply.github.com>
2021-08-11 23:44:43 +0100

    Add tide extremes and sea level requests

--- Sources/StormGlass/Endpoints/Tide/SGTideExtremesPointRequest.swift
+++ Sources/StormGlass/Endpoints/Tide/SGTideExtremesPointRequest.swift
@@ -3,13 +3,47 @@ import Foundation
 public struct SGTideExtremesPointRequest: SGEndpoint {
     public typealias Response = SGTideExtremesPointResponse
 
+    let coordinate: SGCoordinate
+    let startDate: Date?
+    let endDate: Date?
+    let datum: SGTideDatum?
+
     public var path: String {
         "/v2/tide/extremes/point"
     }
 
     public var parameters: [String: SGParameterEncodable?] {
-        [:]
+        [
+            "lat": coordinate.latitude,
+            "lng": coordinate.longitude,
+            "start": startDate,
+            "end": endDate,
+            "datum": datum,
+        ]
     }
 }
 
-public struct SGTideExtremesPointResponse: Decodable {}
+public enum SGTideDatum: String, SGParameterEncodable {
+    case meanLowerLowWater = "mllw"
+    case meanSeaLevel = "msl"
+
+    public func encode() -> String { rawValue }
+}
+
+public struct SGTideExtremesPointResponse: Decodable {
+    public enum ExtremeType: String, Decodable {
+        case low, high
+    }
+
+    public struct Item: Decodable {
+        /// Timestamp in UTC
+        let time: Date
+        /// Height in meters
+        let height: String
+        /// Type of extreme. Either low or high
+        let type: ExtremeType
+    }
+
+    public let data: [Item]
+    public let meta: SGResponseMetadata
+}
--- Sources/StormGlass/Endpoints/Tide/SGTideSeaLevelPointRequest.swift
+++ Sources/StormGlass/Endpoints/Tide/SGTideSeaLevelPointRequest.swift
@@ -3,13 +3,62 @@ import Foundation
 public struct SGTideSeaLevelPointRequest: SGEndpoint {
     public typealias Response = SGTideSeaLevelPointResponse
 
+    let coordinate: SGCoordinate
+    let startDate: Date?
+    let endDate: Date?
+    let datum: SGTideDatum?
+
     public var path: String {
         "/v2/tide/sea-level/point"
     }
 
     public var parameters: [String: SGParameterEncodable?] {
-        [:]
+        [
+            "lat": coordinate.latitude,
+            "lng": coordinate.longitude,
+            "start": startDate,
+            "end": endDate,
+            "datum": datum,
+        ]
     }
 }
 
-public struct SGTideSeaLevelPointResponse: Decodable {}
+public struct SGTideSeaLevelPointResponse: Decodable {
+    public struct Item: Decodable {
+        private struct FailableDouble: Decodable {
+            fileprivate let value: Double?
+
+            init(from decoder: Decoder) throws {
+                let container = try decoder.singleValueContainer()
+                value = try? container.decode(Double.self)
+            }
+        }
+
+        private enum CodingKeys: String, CodingKey {
+            case time
+        }
+
+        /// Timestamp in UTC
+        let time: Date
+        /// Sea level given in metres where the key itself is the name of the source
+        let data: [SGDataSource: Double]
+
+        public init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            let rawData = try decoder.singleValueContainer().decode([String: FailableDouble].self)
+            var builder = [SGDataSource: Double]()
+
+            rawData.forEach { key, value in
+                if let dataSource = SGDataSource(rawValue: key), let underlyingValue = value.value {
+                    builder[dataSource] = underlyingValue
+                }
+            }
+
+            time = try container.decode(Date.self, forKey: .time)
+            data = builder
+        }
+    }
+
+    public let data: [Item]
+    public let meta: SGResponseMetadata
+}
