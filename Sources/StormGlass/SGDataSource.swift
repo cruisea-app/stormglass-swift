26e6c30617bcb52cc36edc31dc2bedd9bda96cc3
James Sherlock <15193942+Sherlouk@users.noreply.github.com>
2021-08-12 00:18:59 +0100

    Add information to data source

--- Sources/StormGlass/SGDataSource.swift
+++ Sources/StormGlass/SGDataSource.swift
@@ -10,6 +10,60 @@ public enum SGDataSource: String, CaseIterable, Decodable {
     case sg
     case mercator // bio only
     case unknown
+
+    public var description: String {
+        switch self {
+        case .icon:
+            return "Germany’s National Meteorological Service, the Deutscher Wetterdienst"
+        case .noaa:
+            return "National Oceanic and Atmospheric Administration"
+        case .meteo:
+            return "French National Meteorological service"
+        case .meto:
+            return "United Kingdom’s national weather service, The UK MetOffice"
+        case .fcoo:
+            return "Danish Defence Centre for Operational Oceanography"
+        case .fmi:
+            return "The Finnish Meteorological Institution"
+        case .yr:
+            return "Norwegian Meteorological Institute and NRK"
+        case .smhi:
+            return "Swedish Meteorological and Hydrological Institute"
+        case .sg:
+            return "Handpicked local forecast in each geographical area by Storm Glass"
+        case .mercator:
+            return "Mercator"
+        case .unknown:
+            return "Unknown Data Source"
+        }
+    }
+    
+    public var updateFrequency: Int? {
+        switch self {
+        case .icon:
+            return 12
+        case .noaa:
+            return 6
+        case .meteo:
+            return 24
+        case .meto:
+            return 24
+        case .fcoo:
+            return 12
+        case .fmi:
+            return 4
+        case .yr:
+            return 4
+        case .smhi:
+            return 12
+        case .sg:
+            return 4
+        case .mercator:
+            return nil
+        case .unknown:
+            return nil
+        }
+    }
 }
 
 public struct SGDataSourceValue<Value: Decodable>: Decodable {
